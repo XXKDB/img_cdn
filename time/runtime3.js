74991413f514ed59f53ac8f72a6d14de75d317b7
XXKDB <82255597+XXKDB@users.noreply.github.com>
2022-02-08 22:26:50 +0800

    Update runtime3.js

--- time/runtime3.js
+++ time/runtime3.js
@@ -26,13 +26,18 @@
     if (second > 0) {
       time[4] = nol(second);
     }
-    if ((Number(time[2])<22) && (Number(time[2])>7)){
+    if ((Number(time[2])<18) && (Number(time[2])>=7)){
       currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/小破站-营业中-6adea8?style=social&logo=cakephp' title='距离百年老店也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
     }
     else{
       currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/小破站-打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
     }
     document.getElementById("workboard").innerHTML = currentTimeHtml;
+    var me2022 = document.getElementsByClassName("v")[0].dataset;
+    if (me2022.class==="v") {
+      document.querySelector('.v[data-class=v] .vactions label.vaction').parentNode.removeChild(document.querySelector('.vactions label.vaction'));
+      document.querySelector('.v[data-class=v] .vactions .upload').parentNode.removeChild(document.querySelector('.vactions .upload'));
+    }
   }, 1000);
 
 
