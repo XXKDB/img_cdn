3c516193a1ef61e57a08c029f08925bbb1c660ea
XXKDB <82255597+XXKDB@users.noreply.github.com>
2022-02-09 14:55:22 +0800

    Update runtime3.js

--- time/runtime3.js
+++ time/runtime3.js
@@ -33,10 +33,10 @@
       currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/小破站-打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
     }
     document.getElementById("workboard").innerHTML = currentTimeHtml;
-    if (document.getElementById("waline-image-upload")) {
-      document.querySelector('.v[data-class=v] .vactions label.vaction').parentNode.removeChild(document.querySelector('.vactions label.vaction'));
-      document.querySelector('.v[data-class=v] .vactions .upload').parentNode.removeChild(document.querySelector('.vactions .upload'));
-    }
+//     if (document.getElementById("waline-image-upload")) {
+//       document.querySelector('.v[data-class=v] .vactions label.vaction').parentNode.removeChild(document.querySelector('.vactions label.vaction'));
+//       document.querySelector('.v[data-class=v] .vactions .upload').parentNode.removeChild(document.querySelector('.vactions .upload'));
+//     }
   }, 1000);
 
 
